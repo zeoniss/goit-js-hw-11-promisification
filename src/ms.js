d3bf46ca2c3942f5422137710dfff5a0d6b26fff
Vladimir Kriukov <zeoniss28@gmail.com>
2021-02-09 20:45:29 +0200

    summary

--- src/ms.js
+++ src/ms.js
@@ -11,6 +11,6 @@ const delay = ms => {
 const logger = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
+console.log(delay(2000).then(logger)); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
